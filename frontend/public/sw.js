97546b56bb5f90a637f00db47b5ab10612ce61cb
ayush166 <ayushkumar3936@gmail.com>
2024-01-30 17:30:30 +0530

    feat: cache

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,2 +1,23 @@
-// const  cacheData="v1"
-// self.addEventListener("install",)
+const  cacheData="v1"
+this.addEventListener("install",(event)=>{
+    event.waitUntil(
+        caches.open(cacheData).then((cache)=>{
+            cache.addAll([
+                '/static/js/bundle.js',
+                "/",
+                "/index.html",
+                "/manifest.json",
+                "/favicon.ico",
+                "/logo192.png"
+            ])
+        })
+    )
+})
+
+this.addEventListener("fetch",(event)=>{
+    event.respondWith(
+        caches.match(event.request).then(result=>{
+            if(result) return result
+        })
+    )
+})
